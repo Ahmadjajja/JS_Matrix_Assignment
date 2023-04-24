a296fb6f575f5e7b0ea7a7b3a88363ad64b5d580
Ahmadjajja <ahmadjajja86@gmail.com>
2023-04-24 20:10:12 +0500

    Q1, Q2, Q3, Q4 solved

--- Q1.js
+++ Q1.js
@@ -1,4 +1,4 @@
-// Assume a matrix P m x n  exists. Copy the matrix column by column in a one dimentional vector V
+// Assume a matrix P m x n  exists. Copy the matrix row by row in a one dimentional vector V
 
 // Example: 
 // A = [
@@ -7,7 +7,7 @@
 // [3, 0, 0, -5]
 // ] 
 
-// V = [12, 11, 3, -3, -2, 0,  2, -8, 0, 14, 4, -5] 
+// V = [12, -3, 2, 14,11, -2, -8, 4, 3, 0, 0, -5] 
 
 const P = [
     [12, -3, 2, 14],
@@ -15,13 +15,10 @@ const P = [
     [3, 0, 0, -5]
   ];
   
-  const m = P.length;
-  const n = P[0].length;
-  
   const V = [];
   
-  for (let j = 0; j < n; j++) {
-    for (let i = 0; i < m; i++) {
+  for (let i = 0; i < P.length; i++) {
+    for (let j = 0; j < P[i].length; j++) {
       V.push(P[i][j]);
     }
   }
